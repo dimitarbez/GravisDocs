3d2de22a627492240eecfe0528fb54673bc7532c
Dimitar Bezhanovski <30288047+dimitarbez@users.noreply.github.com>
2023-09-27 11:20:13 +0200

    Added firmware code to the Make page

--- src/components/Pages/Make/Make.tsx
+++ src/components/Pages/Make/Make.tsx
@@ -5,13 +5,30 @@ const Make: React.FC = () => {
     <div className="container mx-auto p-4">
       <h1 className="text-2xl text-center mb-4">Welcome to the Make Page</h1>
 
-      <div className="bg-white  rounded p-6">
+      <div className="bg-white rounded p-6">
+
         <section className="mb-8">
           <h2 className="text-xl font-bold">Code Documentation</h2>
           <p>All the necessary code, comments, and guidelines needed to understand and replicate the Gravis project.</p>
-          <a href="/code-documentation" className="text-blue-600 underline">
-            View Code Documentation
-          </a>
+          
+          <div className="mb-4">
+            <h3 className="text-lg font-semibold">Main Project Code</h3>
+            <a href="/code-documentation" className="text-blue-600 underline">
+              View Main Code Documentation
+            </a>
+          </div>
+
+          <div className="mb-4">
+            <h3 className="text-lg font-semibold">Microcontroller Firmware</h3>
+            <p>Instructions and resources related to the microcontroller firmware used in the Gravis project.</p>
+            <ol className="list-decimal list-inside">
+              <li>Clone the Gravis firmware repository from <a href="https://github.com/dimitarbez/GravisMicrocontroller" className="text-blue-600 underline">here</a>.</li>
+              <li>Follow the installation and setup guide in the README.md file.</li>
+              <li>For any issues, refer to the troubleshooting section or raise an issue on the repo.</li>
+              <li>Ensure your microcontroller is compatible and has the necessary dependencies installed.</li>
+              <li>After setup, upload the firmware to your microcontroller following the provided steps.</li>
+            </ol>
+          </div>
         </section>
 
         <section className="mb-8">
